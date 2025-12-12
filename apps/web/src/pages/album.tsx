abea93dbb007641820c645134629a6b3f0928449
altierawr <104657457+altierawr@users.noreply.github.com>
2025-12-12 11:26:54 +0200

    feat(web): add album page buttons & play functionality

--- apps/web/src/pages/album.tsx
+++ apps/web/src/pages/album.tsx
@@ -1,6 +1,9 @@
 import { useLoaderData, type LoaderFunction } from "react-router";
+import { Button, IconButton } from "design";
 import type { Album } from "../types";
 import SongList from "../components/song-list";
+import { Heart, Play, Share } from "lucide-react";
+import { usePlayerState } from "../store";
 
 const loader: LoaderFunction = async ({ params }) => {
   const data = await fetch(`http://localhost:3003/v1/albums/${params.id}`);
@@ -11,8 +14,11 @@ const loader: LoaderFunction = async ({ params }) => {
 
 const AlbumPage = () => {
   const data = useLoaderData() as { album: Album };
+  const player = usePlayerState((s) => s.player);
 
-  console.log(data.album);
+  const handlePlayClick = async () => {
+    player.playSongs(data.album.songs, 0);
+  };
 
   return (
     <div className="max-w-[900px]">
@@ -24,10 +30,22 @@ const AlbumPage = () => {
           }}
         ></div>
 
-        <div className="flex flex-col">
+        <div className="flex flex-col justify-between items-start">
           <h1 className="text-4xl font-bold tracking-tight">
             {data.album.title}
           </h1>
+          <div className="flex gap-4">
+            <Button color="blue" onClick={handlePlayClick}>
+              <Play size={16} fill="currentColor" />
+              Play Album
+            </Button>
+            <IconButton color="gray" variant="soft">
+              <Heart size={16} />
+            </IconButton>
+            <IconButton color="gray" variant="soft">
+              <Share size={16} />
+            </IconButton>
+          </div>
         </div>
       </div>
       <div className="mt-4"></div>
