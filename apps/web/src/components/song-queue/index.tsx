0bc649e2e7799535770eabe536dee716d36a766d
altierawr <104657457+altierawr@users.noreply.github.com>
2026-01-22 01:17:50 +0200

    style(web): redesign song queue

--- apps/web/src/components/song-queue/index.tsx
+++ apps/web/src/components/song-queue/index.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useRef } from "react";
 import { useGeneralStore, usePlayerState } from "../../store";
 import SongQueueItem from "./item";
+import { Spacer } from "design";
 
 const SongQueue = () => {
   const playerState = usePlayerState();
+  const generalState = useGeneralStore();
   const isVisible = useGeneralStore((store) => store.isSongQueueVisible);
 
   const nowPlayingRef = useRef<HTMLHeadingElement>(null);
@@ -16,62 +18,75 @@ const SongQueue = () => {
     }
   }, [isVisible]);
 
+  const handleBlur = () => {
+    console.log("blur");
+    generalState.setIsSongQueueVisible(false);
+  };
+
   return (
     <div
-      className="fixed right-0 top-0 min-w-[300px] p-4"
+      className="fixed inset-0 w-dvw bg-[rgba(0,0,0,0.3)] z-50 transition-opacity"
       style={{
-        height: "calc(100dvh - 100px)",
         pointerEvents: isVisible ? "unset" : "none",
         opacity: isVisible ? "1" : "0",
-        transform: isVisible ? "translate(0, 0)" : "translate(0, 5px)",
-        transition: "opacity 0.15s ease-out, transform 0.15s ease-out",
+        height: "calc(100dvh - 100px)",
       }}
+      onClick={handleBlur}
     >
-      {isVisible}
-      <div className="w-full h-full bg-(--gray-2) rounded-md flex flex-col border border-(--gray-6)">
-        <div className="p-5">
-          <h2 className="text-(--gray-12) text-lg font-black tracking-wide">
-            Queue
-          </h2>
-        </div>
+      <div
+        className="fixed right-0 top-0 min-w-[300px] p-4"
+        style={{
+          height: "calc(100dvh - 100px)",
+          transform: isVisible ? "translate(0, 0)" : "translate(0, 5px)",
+          transition: "transform 0.15s ease-out",
+        }}
+        onClick={(e) => e.stopPropagation()}
+      >
+        {isVisible}
+        <div className="w-full h-full bg-(--gray-2) rounded-md grid border border-(--gray-6)">
+          <div className="p-5">
+            <h2 className="text-(--gray-12) text-lg font-black tracking-wide">
+              Queue
+            </h2>
+          </div>
 
-        <div className="flex-1 overflow-y-auto flex flex-col px-5">
-          {playerState.player.playlist.length === 0 && <p>Queue empty</p>}
+          <div className="flex-1 overflow-y-auto grid px-5">
+            {playerState.player.playlist.length === 0 && <p>Queue empty</p>}
 
-          {playerState.playInfo?.playlistIndex !== undefined && (
-            <>
-              {playerState.playInfo.playlistIndex > 0 && (
-                <>
-                  <h3 className="font-semibold">Previous tracks</h3>
-                  {playerState.player.playlist
-                    .slice(0, playerState.playInfo.playlistIndex)
-                    .map((pe) => (
-                      <SongQueueItem key={pe.song.id} song={pe.song} />
-                    ))}
-                  <div className="min-h-5" />
-                </>
-              )}
+            {playerState.playInfo?.playlistIndex !== undefined && (
+              <>
+                {playerState.playInfo.playlistIndex > 0 && (
+                  <>
+                    <h3 className="font-semibold">Previous tracks</h3>
+                    {playerState.player.playlist
+                      .slice(0, playerState.playInfo.playlistIndex)
+                      .map((pe) => (
+                        <SongQueueItem key={pe.song.id} song={pe.song} />
+                      ))}
+                    <div className="min-h-5" />
+                  </>
+                )}
 
-              <h3 ref={nowPlayingRef} className="font-semibold">
-                Now playing
-              </h3>
-              <SongQueueItem
-                key={
-                  playerState.player.playlist[
-                    playerState.playInfo.playlistIndex
-                  ].song.id
-                }
-                song={
-                  playerState.player.playlist[
-                    playerState.playInfo.playlistIndex
-                  ].song
-                }
-              />
+                <h3 ref={nowPlayingRef} className="font-semibold">
+                  Now playing
+                </h3>
+                <SongQueueItem
+                  key={
+                    playerState.player.playlist[
+                      playerState.playInfo.playlistIndex
+                    ].song.id
+                  }
+                  song={
+                    playerState.player.playlist[
+                      playerState.playInfo.playlistIndex
+                    ].song
+                  }
+                />
 
-              <div className="min-h-5" />
+                <Spacer size="5" />
 
-              {playerState.player.playlist.length >
-                playerState.playInfo.playlistIndex + 1 && (
+                {playerState.player.playlist.length >
+                  playerState.playInfo.playlistIndex + 1 && (
                   <>
                     <h3 className="font-semibold">Next up</h3>
                     {playerState.player.playlist
@@ -84,8 +99,11 @@ const SongQueue = () => {
                       ))}
                   </>
                 )}
-            </>
-          )}
+
+                <Spacer size="4" />
+              </>
+            )}
+          </div>
         </div>
       </div>
     </div>
--- apps/web/src/components/song-queue/item.tsx
+++ apps/web/src/components/song-queue/item.tsx
@@ -1,30 +1,60 @@
 import { Link } from "react-router";
 import type { Song } from "../../types";
 import { getTidalCoverUrl } from "../../utils/image";
+import CoverBlock, { CoverBlockVariant } from "../music-blocks/cover-block";
+import { useState } from "react";
+import clsx from "clsx";
 
 type TProps = {
   song: Song;
 };
 
 const SongQueueItem = ({ song }: TProps) => {
+  const [isActive, setIsActive] = useState(false);
+
   return (
-    <div className="flex gap-3 hover:bg-(--gray-4) px-3 py-2 rounded-md -ml-3">
+    <div
+      className={clsx(
+        "flex gap-2 hover:bg-(--gray-4) px-3 py-2 rounded-md -ml-3",
+        isActive && "bg-(--gray-5)!",
+      )}
+      onMouseDown={() => setIsActive(true)}
+      onMouseUp={() => setIsActive(false)}
+      onMouseLeave={() => setIsActive(false)}
+    >
       <div
-        className="h-[50px] aspect-square bg-cover rounded-lg"
-        style={{
-          backgroundImage: `url(${
-            song.album?.cover ? getTidalCoverUrl(song.album.cover, 80) : ""
-          })`,
-        }}
-      />
+        className="h-[45px] aspect-square"
+        onMouseDown={(e) => e.stopPropagation()}
+      >
+        <CoverBlock
+          variant={CoverBlockVariant.PLAY_ONLY}
+          imageUrl={getTidalCoverUrl(song.album?.cover, 80)}
+        />
+      </div>
 
       <div className="flex flex-col justify-center">
-        <p className="font-normal">{song.title}</p>
-        {song.artists.map((artist) => (
-          <Link key={artist.id} to={`/artists/${artist.id}`}>
-            {artist.name}
-          </Link>
-        ))}
+        <p
+          className="text-sm font-bold line-clamp-2"
+          onMouseDown={(e) => e.stopPropagation()}
+        >
+          {song.album && (
+            <Link to={`/albums/${song.album.id}?song=${song.id}`}>
+              {song.title}
+            </Link>
+          )}
+          {!song.album && song.title}
+        </p>
+        <p
+          className="text-xs text-(--gray-11) line-clamp-1"
+          onMouseDown={(e) => e.stopPropagation()}
+        >
+          {song.artists.map((artist, index) => (
+            <span key={artist.id}>
+              <Link to={`/artists/${artist.id}`}>{artist.name}</Link>
+              {index < song.artists.length - 1 && ", "}
+            </span>
+          ))}
+        </p>
       </div>
     </div>
   );
