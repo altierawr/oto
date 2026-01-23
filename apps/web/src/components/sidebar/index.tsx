39bf07d5bbea3f0ea79b0b61a793d5fb2f111524
altierawr <104657457+altierawr@users.noreply.github.com>
2026-01-23 17:09:19 +0200

    chore(web): remove sidebar links

--- apps/web/src/components/sidebar/index.tsx
+++ apps/web/src/components/sidebar/index.tsx
@@ -1,28 +1,6 @@
-import { IconHome, IconUser, IconAlbum } from "@tabler/icons-react";
-import SidebarLink from "./sidebar-link";
-
 const Sidebar = () => {
   return (
-    <div className="h-full min-w-[220px] w-[220px] pt-8 px-2 sticky">
-      <SidebarLink
-        icon={<IconHome size={20} stroke={1.5} />}
-        text="Home"
-        url="/"
-        location="home"
-      />
-      <SidebarLink
-        icon={<IconUser size={20} stroke={1.5} />}
-        text="Artists"
-        url="/artists"
-        location="artists"
-      />
-      <SidebarLink
-        icon={<IconAlbum size={20} stroke={1.5} />}
-        text="Albums"
-        url="/albums"
-        location="albums"
-      />
-    </div>
+    <div className="h-full min-w-[220px] w-[220px] pt-8 px-2 sticky"></div>
   );
 };
 
