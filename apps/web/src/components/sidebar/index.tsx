8b791983107c9ae2c6c4ddb9233f5832a044e64f
altierawr <104657457+altierawr@users.noreply.github.com>
2026-01-31 17:51:32 +0200

    feat(web): add app logo

--- apps/web/src/components/sidebar/index.tsx
+++ apps/web/src/components/sidebar/index.tsx
@@ -5,7 +5,7 @@ import { Facehash } from "facehash";
 import { ChevronUp } from "lucide-react";
 import { Menu } from "design";
 import { request } from "../../utils/http";
-import { useNavigate } from "react-router";
+import { Link, useNavigate } from "react-router";
 import { useState } from "react";
 import clsx from "clsx";
 
@@ -39,46 +39,53 @@ const Sidebar = () => {
   };
 
   return (
-    <div className="h-full min-w-(--sidebar-width) w-(--sidebar-width) py-4 px-2 grid items-end border-r border-r-(--gray-3)">
-      {user && (
-        <Menu.Root onOpenChange={(open) => setIsUserMenuOpen(open)}>
-          <Menu.Trigger>
-            <div className="flex items-center justify-between py-2 px-3 rounded-md transition-colors group hover:bg-(--gray-4) cursor-pointer">
-              <div className="flex gap-2 items-center w-full">
-                <Facehash
-                  className="rounded-md select-none"
-                  name={user.username}
-                  colors={["#264653", "#2a9d8f", "#e9c46a"]}
-                  size={32}
-                />
+    <div className="h-full min-w-(--sidebar-width) w-(--sidebar-width) py-4 px-2 flex flex-col justify-between border-r border-r-(--gray-3)">
+      <Link to="/" className="flex items-center px-3 gap-3 no-underline!">
+        <img src={"/logo-64.png"} width="20" height="20" />
+        <p className="font-medium text-2xl">oto</p>
+      </Link>
 
-                <div className="overflow-hidden flex-1 grid items-center">
-                  <p className="text-(--gray-12) text-sm max-w-[100px] truncate text-left">
-                    {user.username}
-                  </p>
-                  <p className="text-(--gray-11) text-xs line-clamp-1 text-left">
-                    {user.isAdmin ? "Admin user" : "Regular user"}
-                  </p>
+      <div className="w-full *:w-full">
+        {user && (
+          <Menu.Root onOpenChange={(open) => setIsUserMenuOpen(open)}>
+            <Menu.Trigger>
+              <div className="flex items-center justify-between py-2 px-3 rounded-md transition-colors group hover:bg-(--gray-4) cursor-pointer">
+                <div className="flex gap-2 items-center w-full">
+                  <Facehash
+                    className="rounded-md select-none"
+                    name={user.username}
+                    colors={["#264653", "#2a9d8f", "#e9c46a"]}
+                    size={32}
+                  />
+
+                  <div className="overflow-hidden flex-1 grid items-center">
+                    <p className="text-(--gray-12) text-sm max-w-[100px] truncate text-left">
+                      {user.username}
+                    </p>
+                    <p className="text-(--gray-11) text-xs line-clamp-1 text-left">
+                      {user.isAdmin ? "Admin user" : "Regular user"}
+                    </p>
+                  </div>
+
+                  <ChevronUp
+                    size={16}
+                    className={clsx(
+                      "text-(--gray-11) group-hover:text-(--gray-12) transition-[rotate,color] rotate-0",
+                      isUserMenuOpen && "rotate-180",
+                    )}
+                  />
                 </div>
-
-                <ChevronUp
-                  size={16}
-                  className={clsx(
-                    "text-(--gray-11) group-hover:text-(--gray-12) transition-[rotate,color] rotate-0",
-                    isUserMenuOpen && "rotate-180",
-                  )}
-                />
               </div>
-            </div>
-          </Menu.Trigger>
-          <Menu.Content>
-            <Menu.Item onClick={handleChangePassword}>
-              Change password
-            </Menu.Item>
-            <Menu.Item onClick={handleLogOut}>Log out</Menu.Item>
-          </Menu.Content>
-        </Menu.Root>
-      )}
+            </Menu.Trigger>
+            <Menu.Content>
+              <Menu.Item onClick={handleChangePassword}>
+                Change password
+              </Menu.Item>
+              <Menu.Item onClick={handleLogOut}>Log out</Menu.Item>
+            </Menu.Content>
+          </Menu.Root>
+        )}
+      </div>
     </div>
   );
 };
