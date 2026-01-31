39110d07cef6150e645c52901e3fd5d27974c87e
altierawr <104657457+altierawr@users.noreply.github.com>
2026-02-01 00:31:28 +0200

    fix(web): ensure we use local react and react-dom packages

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      react: path.resolve(__dirname, "./node_modules/react"),
+      "react-dom": path.resolve(__dirname, "./node_modules/react-dom"),
+    },
+  },
   optimizeDeps: {
     exclude: ["design"], // Don't pre-bundle the linked package
   },
