75e63d14371217cc86e152997732e1c4ee9f2ba4
altierawr <104657457+altierawr@users.noreply.github.com>
2026-02-03 16:15:14 +0200

    chore(web): improve stream error response log message

--- apps/web/src/music-player.ts
+++ apps/web/src/music-player.ts
@@ -1897,7 +1897,7 @@ export class MusicPlayer {
         { cache: "no-store" },
       );
       if (streamResp.status !== 201) {
-        console.error("woops");
+        console.error("Stream creation was unsuccesful");
         return;
       }
 
